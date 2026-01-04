9a4a73e4017471e89bff14e6056d97941c598634
Radif Sharafullin <radif@replaydigital.net>
2026-01-03 16:35:42 -0800

    Initial Commit

---
+++ MyWorkouts Watch App/MyWorkoutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWorkoutsApp.swift
+//  MyWorkouts Watch App
+//
+//  Created by Radif Sharafullin on 1/3/26.
+//
+
+import SwiftUI
+
+@main
+struct MyWorkouts_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
